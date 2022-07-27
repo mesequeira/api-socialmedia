2523f9e9688e620e47604f6175eb955e6e152698
markes41 <40794902+markes41@users.noreply.github.com>
2022-07-27 19:42:03 -0300

    Add a handle validation.

---
+++ Application/Exceptions/ValidationException.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Exceptions
+{
+    public class ValidationException : Exception
+    {
+
+        public ValidationException() : base("A validation error ocurred trying to consume the application.")
+        {
+            Errors = new List<string>();
+        }
+
+        public List<string> Errors { get; }
+
+        public ValidationException(IEnumerable<ValidationFailure> failures) : this()
+        {
+            foreach (var failure in failures)
+                Errors.Add(failure.ErrorMessage);
+        }
+
+    }
+}
